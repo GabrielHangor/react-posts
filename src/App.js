7488309a0bb3355597b5bb6a3a197553a57f5824
Gabriel Khabibulin <gabrielkhabibulin@MacBook-Air-Gabriel.local>
2021-11-07 20:36:54 +0300

    basic project layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>Javascript - язык программирования</div>
+        </div>
+      </div>
+      <div className="post__btns">
+        <button>Удалить </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
