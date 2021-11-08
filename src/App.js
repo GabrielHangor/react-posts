376a842d0382a9b9f49e0d579773cc896a39d8af
Gabriel Khabibulin <gabrielkhabibulin@MacBook-Air-Gabriel.local>
2021-11-08 14:53:39 +0300

    components/styles library layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import './styles/App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript', body: 'Description' },
+    { id: 3, title: 'Javascript', body: 'Description' },
+  ]);
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>Javascript - язык программирования</div>
-        </div>
-      </div>
-      <div className="post__btns">
-        <button>Удалить </button>
-      </div>
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <MyButton>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title={'Список постов'} />
     </div>
   );
 }
