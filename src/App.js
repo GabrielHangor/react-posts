ee44263cc78431bc380c1805fc40ce57b425198f
Gabriel <gaba93@yandex.ru>
2021-11-15 13:33:06 +0300

    usePosts hook, decomposition

--- src/App.js
+++ src/App.js
@@ -4,44 +4,15 @@ import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyModal from './components/UI/MyModal/MyModal';
+import { usePosts } from './hooks/usePost';
 
 import './styles/App.css';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'Javascript',
-      body: 'Динамически типизированный язык программирования',
-    },
-    {
-      id: 2,
-      title: 'Java',
-      body: 'Статически типизированный язык программирования',
-    },
-    {
-      id: 3,
-      title: 'C#',
-      body: 'Более низкоуровневый язык',
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
